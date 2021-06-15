8f7e8eeb57378cf1c64c421342b7064b94abbc6b
Thomas McNeal <mcneal.tm21@gmail.com>
2021-06-14 23:11:14 -0500

    Add functions to view and hide amortization schedule

--- static/js/index.js
+++ static/js/index.js
@@ -6,9 +6,23 @@ $(document).ready(function() {
             yearsToRepay: $("#txtYearsToRepay").val()
         }, function(data) {
             $("#monthlyPayment").html("$" + data.result);
-            $("#scheduleViewButton").html('<button id="btnSchedule">View Amortization Schedule<\/button>');
+            $("#scheduleViewButton").html('<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button>');
             document.getElementById("scheduleTable").innerHTML = data.schedule;
         });
         return false;
     });
 });
+
+
+
+function viewSchedule() {
+    document.getElementById("scheduleDataFrame").style.display = "inline-block";
+    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="hideSchedule();">Hide Amortization Schedule<\/button>'
+}
+
+function hideSchedule() {
+    document.getElementById("scheduleDataFrame").style.display = "none";
+    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button>'
+
+}
+
