15fd2cea2fd019a7c93d116d39a0cc5476b2616a
Thomas McNeal <mcneal.tm21@gmail.com>
2021-06-16 09:38:33 -0500

    Add styling

--- static/js/index.js
+++ static/js/index.js
@@ -6,23 +6,25 @@ $(document).ready(function() {
             yearsToRepay: $("#txtYearsToRepay").val()
         }, function(data) {
             $("#monthlyPayment").html("$" + data.result);
-            $("#scheduleViewButton").html('<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button>');
-            document.getElementById("scheduleTable").innerHTML = data.schedule;
+            $("#scheduleViewButton").html('<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button><p><i class="arrow up"></i></p>');
+            $("#scheduleTable").html('<h2 id="scheduleHeader">Amortization Schedule<h2>' + data.schedule);
+            document.getElementById("scheduleHeader").style.display = "none"; 
+            document.getElementById("scheduleDataFrame").style.display = "none"; // data.schedule ID selector is scheduleDataFrame
         });
         return false;
     });
 });
 
-
-
 function viewSchedule() {
-    document.getElementById("scheduleDataFrame").style.display = "inline-block";
-    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="hideSchedule();">Hide Amortization Schedule<\/button>'
+    document.getElementById("scheduleDataFrame").style.display = "table";
+    document.getElementById("scheduleHeader").style.display = "block";
+    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="hideSchedule();">Hide Amortization Schedule<\/button><p><i class="arrow down"></i></p>'
 }
 
 function hideSchedule() {
     document.getElementById("scheduleDataFrame").style.display = "none";
-    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button>'
+    document.getElementById("scheduleHeader").style.display = "none";
+    document.getElementById("scheduleViewButton").innerHTML = '<button id="btnSchedule" onclick="viewSchedule();">View Amortization Schedule<\/button><p><i class="arrow up"></i></p>'
 
 }
 
